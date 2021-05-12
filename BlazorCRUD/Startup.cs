b7bbe098444b8b101663b8570b978e257dd7366b
apra984 <62280873+apra984@users.noreply.github.com>
2021-05-12 14:48:09 +1200

    Update Startup.cs

--- BlazorCRUD/Startup.cs
+++ BlazorCRUD/Startup.cs
@@ -17,7 +17,7 @@ namespace BlazorCRUD
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration
         }
 
         public IConfiguration Configuration { get; }
